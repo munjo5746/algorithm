caf5ea6c7725ef5470af13a7e023a810c6a74ec0
Edward <munjo5746@gmail.com>
2015-08-11 01:19:30 -0400

    partial derivatives are calculated and the error values are caculated

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -16,7 +16,21 @@ class Regressor():
         N = len(self.data) # data will be assumed as set of pairs.[(x_0,y_0), ...]
         S = 0
         error = 0
+        estimated = 0
         for (x,y) in data:
-            S += (y - (slope * x) + b)**2
+            estimated = (slope * x) + b
+            S += (y - estimated)**2
         error = float(S)/N
         return error
+
+    def computeMinError(self, slope, yIntercept, alpha):
+
+        respSlope = 0
+        respYIntercept = 0
+        N = len(data)
+        for (x,y) in data:
+            respSlope += (y - (slope * x + yIntercept)) * (-1 * x)
+            respYIntercept += (-1 * (y - (slope * x + b)))
+        newSlope = slope - (alpha * respSlope)
+        newYIntercept = yIntercept - (alpha * respYIntercept)
+        return (newSlope, newYIntercept)
